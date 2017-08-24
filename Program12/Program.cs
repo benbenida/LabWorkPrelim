15f38d3e3b269d9dce319bca8159f232f127ff1f
unknown <clyd.benidz@gmail.com>
2017-08-24 22:37:43 +0800

    Second Commit

--- Program12/Program.cs
+++ Program12/Program.cs
@@ -10,7 +10,17 @@ namespace Program12
 	{
 		static void Main(string[] args)
 		{
+            Console.Write("Please enter a year: ");
+            int yearInput = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i <= 20; i++)
+            {
+                if ((yearInput % 4 == 0) && (yearInput % 100 != 0))
+                {
+                    Console.WriteLine(yearInput);
+                }
+                yearInput++;
+            }
 		}
 	}
 }
